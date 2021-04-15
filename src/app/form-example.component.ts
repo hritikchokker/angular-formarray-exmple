210e8463075ccff981a704987321fa43f1e0e2b4
hritikchokker <hchokker7@gmail.com>
2021-04-15 21:16:42 +0530

    3 files modified, 6 added, 1 deleted

---
+++ src/app/form-example.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { FormArray, FormBuilder, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-forms",
+  templateUrl: "./form-example.component.html",
+  styleUrls: ["./form-example.component.css"]
+})
+export class FormExampleComponent {
+  form;
+  constructor(private _fb: FormBuilder) {
+    this.createForm();
+  }
+  createForm() {
+    this.form = this._fb.group({
+      name: [""],
+      age: [""],
+      technologyStack: this._fb.array([])
+    });
+  }
+
+  get technologyStack() {
+    return this.form.controls["technologyStack"] as FormArray;
+  }
+
+  addDynamicControl() {
+    const controlaDD = this._fb.group({
+      title: ["", Validators.required],
+      description: ["beginner", Validators.required]
+    });
+    console.log(controlaDD, "sss");
+    this.technologyStack.push(controlaDD);
+  }
+  deleteTechnology(lessonIndex: number) {
+    this.technologyStack.removeAt(lessonIndex);
+  }
+}
